716a9a9320cea7bed33b900ab4bf6cb85e4f65a3
Hector Sabillon <h.hsabillon@gmail.com>
2018-09-27 10:49:51 -0600

    Comienzo del formulario ExecuteReader.

---
+++ EjecutarComandos/EjecutarComandos/ExecuteReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace Ejecutar_comandos
+{
+    public partial class ExecuteReader : Form
+    {
+        public ExecuteReader()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteReader_Load(object sender, EventArgs e)
+        {
+            // Crear la conexión
+            SqlConnection conn = new SqlConnection(@"server = (local)\sqlexpress;
+                integrated security = true; database = AdventureWorks2014;");
+
+            // Crear el query
+            string sql = @"SELECT Name, ProductNumber
+                           FROM Production.Product";
+
+            // Crear el comando
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+
+            }
+            catch (SqlException ex)
+            {
+                
+            }
+        }
+    }
+}
