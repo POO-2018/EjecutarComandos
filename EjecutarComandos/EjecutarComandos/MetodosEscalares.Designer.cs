85c2743d5e6b423e8d5284c4dd1f7f28e4e0bab3
Hector Sabillon <h.hsabillon@gmail.com>
2018-09-27 10:32:40 -0600

    Creación del formulario de Métodos Escalares

---
+++ EjecutarComandos/EjecutarComandos/MetodosEscalares.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ejecutar_comandos
+{
+    partial class MetodosEscalares
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTotalFilas = new System.Windows.Forms.Label();
+            this.txtEscalar = new System.Windows.Forms.TextBox();
+            this.btnTotalFilas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTotalFilas
+            // 
+            this.lblTotalFilas.AutoSize = true;
+            this.lblTotalFilas.Location = new System.Drawing.Point(8, 67);
+            this.lblTotalFilas.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTotalFilas.Name = "lblTotalFilas";
+            this.lblTotalFilas.Size = new System.Drawing.Size(187, 25);
+            this.lblTotalFilas.TabIndex = 0;
+            this.lblTotalFilas.Text = "Total de Registros";
+            // 
+            // txtEscalar
+            // 
+            this.txtEscalar.Location = new System.Drawing.Point(210, 23);
+            this.txtEscalar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtEscalar.Multiline = true;
+            this.txtEscalar.Name = "txtEscalar";
+            this.txtEscalar.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtEscalar.Size = new System.Drawing.Size(324, 121);
+            this.txtEscalar.TabIndex = 1;
+            // 
+            // btnTotalFilas
+            // 
+            this.btnTotalFilas.Location = new System.Drawing.Point(550, 58);
+            this.btnTotalFilas.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnTotalFilas.Name = "btnTotalFilas";
+            this.btnTotalFilas.Size = new System.Drawing.Size(216, 44);
+            this.btnTotalFilas.TabIndex = 2;
+            this.btnTotalFilas.Text = "Contar Registros";
+            this.btnTotalFilas.UseVisualStyleBackColor = true;
+            this.btnTotalFilas.Click += new System.EventHandler(this.btnTotalFilas_Click);
+            // 
+            // MetodosEscalares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 175);
+            this.Controls.Add(this.btnTotalFilas);
+            this.Controls.Add(this.txtEscalar);
+            this.Controls.Add(this.lblTotalFilas);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MetodosEscalares";
+            this.Text = "Métodos Escalares";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTotalFilas;
+        private System.Windows.Forms.TextBox txtEscalar;
+        private System.Windows.Forms.Button btnTotalFilas;
+    }
+}
---
+++ EjecutarComandos/EjecutarComandos/MetodosEscalares.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace Ejecutar_comandos
+{
+    public partial class MetodosEscalares : Form
+    {
+        public MetodosEscalares()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTotalFilas_Click(object sender, EventArgs e)
+        {
+            // Crear la conexión
+            SqlConnection conn = new SqlConnection(@"server = (local)\sqlexpress;
+                integrated security = true; database = AdventureWorks2014;");
+
+            // Crear el query scalar
+            string sql = @"SELECT COUNT(*)
+                           FROM Production.Product";
+
+            // Crear el comando
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+                // Establecer la conexión
+                conn.Open();
+
+                txtEscalar.AppendText("Comando creado y conexión establecida\n.");
+                txtEscalar.AppendText("El número de productos en Production.Product es: ");
+
+                // Ejecutar el query scalar
+                txtEscalar.AppendText(cmd.ExecuteScalar().ToString());
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace, "¡Detalles del error!");
+            }
+            finally
+            {
+                // Cerrar la conexión
+                conn.Close();
+                txtEscalar.AppendText("\nConexión cerrada.");
+            }
+        }
+    }
+}
